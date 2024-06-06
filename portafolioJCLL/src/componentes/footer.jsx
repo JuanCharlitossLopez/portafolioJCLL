683cd1912fce0adc1025228d4b62a7a56ad105dc
JuanCharlitossLopez <juancalopez825@gmail.com>
2024-06-05 21:01:44 -0600

    Actualizacion de cambios en el Diseño Se agregaron nuevos colores y logo.

--- portafolioJCLL/src/componentes/footer.jsx
+++ portafolioJCLL/src/componentes/footer.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import logo from '../assets/logoJC.jpg'
 
+const stylo = {
+  estiloFooter: {
+    textAlign: "center",
+    padding: "20px",
+    backgroundColor: "#1680a4",
+  },
+};
 export const Footer = () => {
   return (
     <>
-    <hr />
-    <footer style={{textAlign:'center'}}>
-      <p>© Copyright. <strong>Create by Juan Carlos Lopez Leon</strong></p>
-      
-    </footer>
+      <footer style={stylo.estiloFooter}>
+        <hr style={{color:'white'}}/>
+        <p style={{ background: "none"}}> <img src={logo} width={50} alt="ERROR" style={{marginRight:'2rem', markerEnd:'3rem'}}/>
+          © Copyright <strong>Create by Juan Carlos López León</strong>
+        </p>
+      </footer>
     </>
-  )
-}
+  );
+};
--- portafolioJCLL/src/componentes/navbar.jsx
+++ portafolioJCLL/src/componentes/navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import '../styles/navbar.css'
+import logoName from '../assets/logoJC.jpg'
+
 
 export const Nav = () => {
     return (
         <>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark" >
+            {/* <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link> */}
+            <nav class="navbar navbar-expand-lg" data-bs-theme="dark" style={{background:'#1680a4'}} >
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Juan Carlos L.L</a>
+                        {/* <a class="navbar-brand" href="#">Juan Carlos L.L</a> */}
+                        <img src={logoName} alt="" width={50} />
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -17,7 +20,7 @@ export const Nav = () => {
                                 <a class="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Proyectos</a>
+                                <a class="nav-link" href="">Proyectos</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="#">Biografia</a>
@@ -29,7 +32,7 @@ export const Nav = () => {
                     </div>
                 </div>
             </nav>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+            {/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> */}
         </>
     )
 }
